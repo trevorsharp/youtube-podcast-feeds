8d851d7f2eeb582c57a821d6c79ee37b9ac3d5ca
Trevor Sharp <sharp.trevor@me.com>
2020-06-16 19:18:57 -0700

    Add config

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  hostname: 'https://yourdomain.com',
+  workingDirectory: './data',
+  apiKey: 'put-your-youtube-api-key-here',
+  fetchLimit: 5,
+  feeds: [
+    {
+      id: 'h3podcast',
+      title: 'H3 Podcast',
+      channel: 'UCLtREJY21xRfCuEKvdki1Kw',
+      regex: 'H3 Podcast #[0-9]+',
+    },
+  ],
+};
