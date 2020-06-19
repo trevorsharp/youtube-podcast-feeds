a51662aa18da2b836d15cda0e08017cc1f53ecb7
Trevor Sharp <sharp.trevor@me.com>
2020-06-19 06:43:53 -0700

    Remove highest video download

--- src/utilities/downloader.js
+++ src/utilities/downloader.js
@@ -1,14 +1,9 @@
 const fs = require('fs');
 const ytdl = require('ytdl-core');
-const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
-const ffmpeg = require('fluent-ffmpeg');
 const logger = require('./logger');
-
-ffmpeg.setFfmpegPath(ffmpegPath);
 
 const downloadContent = async (videoId, directory) => {
   const file = `${directory}/${videoId}.mp4`;
-  const videoOnlyFile = `${directory}/video-${videoId}.mp4`;
 
   const url = `http://www.youtube.com/watch?v=${videoId}`;
 
@@ -19,29 +14,11 @@ const downloadContent = async (videoId, directory) => {
 
     logger.log(`Starting Download for ${videoId}`);
     ytdl(url, {
-      quality: 'highestaudio',
+      quality: 'highest',
       filter: (format) => format.container === 'mp4',
     })
       .pipe(fs.createWriteStream(file))
-      .on('finish', () => {
-        logger.log(`Audio Downloaded for ${videoId}`);
-        const video = ytdl(url, {
-          quality: 'highestvideo',
-          filter: (format) => format.container === 'mp4',
-        });
-        ffmpeg()
-          .input(video)
-          .videoCodec('copy')
-          .input(file)
-          .audioCodec('copy')
-          .save(videoOnlyFile)
-          .on('error', console.error)
-          .on('end', () => {
-            logger.log(`Video Downloaded for ${videoId}`);
-            fs.unlinkSync(file);
-            fs.renameSync(videoOnlyFile, file);
-          });
-      });
+      .on('finish', () => logger.log(`Download Complete for ${videoId}`));
   }
 };
 
